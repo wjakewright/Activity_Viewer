dee7b038e49e2f276b2c114788c98214b889ea09
Jake Wright <wrightwj8@gmail.com>
2021-10-20 13:10:30 -0700

    Updating image display

--- GUI_template.py
+++ GUI_template.py
@@ -6,6 +6,8 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 from PIL import ImageTk, Image
+import skimage as ski
+import numpy as np
 
 
 def Activity_Viewer():
@@ -18,35 +20,53 @@ def Activity_Viewer():
     '''
     
     root = Tk.Tk()
+    # Set window location and size relative to computer
+    # screen size
+    sc_w = int(root.winfo_screenwidth() *0.8)
+    sc_h = int(root.winfo_screenheight() *0.8)
+    c_x = int(root.winfo_screenwidth()/2 - sc_w/2)
+    c_y = int(root.winfo_screenheight()/2 - sc_h/2)
+    #win_size = str(sc_w*.9) +'x' + str(sc_h*.9)
     root.title('Activity Viewer')
-    root.geometry('500x500')
-
+    root.geometry(f'{sc_w}x{sc_h}+{c_x}+{c_y}') #wxh
     
+    # Set canvas for image display
+    image_window = Tk.Frame(root)
+    image_window.grid(row=1,column=1)
+            
+    
+    def Update_Image(event):
+        return
+    
+    def display_tif(filename):
+        tif_stack = ski.io.imread(filename)
+        tif_list = []
+        for tif in range(np.shape(tif_stack)[0]):
+            im = Image.fromarray(tif_stack[tif,:,:])
+            img = ImageTk.PhotoImage(im)
+            tif_list.append(img)
+        slider = Tk.Scale(image_window,from_=0,to=len(tif_list),
+                          orient='horizontal',command=Update_Image)
+        return tif_list
+        
     
     def select_image_files():
         #filetypes = (('tif files','*.tif'))
-        filename = fd.askopenfilename(title='Select File',initialdir='/Users/williamwright/Desktop/')
-        #showinfo(title='Selected File',message=filename)
-        im = ImageTk.PhotoImage(Image.open(filename))
+        filename = fd.askopenfilename(title='Select File',initialdir=r'C:\Users\Jake\Desktop\python_code\Activity_Viewer')
+        tif_list = display_tif(filename)
         ## Weird Tkinter behavior. Must keep a reference of the image object in the widget
         ## for Tkinter to use for further reference. Otherwise it destroys it.
-        panel = Tk.Label(root,image=im)
-        panel.image = im
+        panel = Tk.Label(image_window,image=tif_list[55])
+        panel.image = tif_list[55]
         panel.pack()
         
     
     
     open_button = ttk.Button(root,text='Select a File',command=select_image_files)
 
-    open_button.pack(expand=True)
+    open_button.grid(column=0,row=0)
     
-    
-    
-    
-    
-    
-    
-    
+
     
     root.mainloop()
     
