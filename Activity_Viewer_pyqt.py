8dd994f6d8c85bf955a4b5af8ae78d4253201eea
Jake Wright <wrightwj8@gmail.com>
2021-11-11 07:56:03 -0800

    Added some configuration parameters

--- Activity_Viewer_pyqt.py
+++ Activity_Viewer_pyqt.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5 import QtWidgets as Widgets
+import pyqtgraph as pg
+from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from . import menus 
+
 
 
 
@@ -14,13 +17,26 @@ class Activity_Viewer(QMainWindow):
             William (Jake) Wright - 11/10/2021  ''' 
             
     def __init__(self):
+        '''Initialize class and set up some parameters '''
         super(Activity_Viewer,self).__init__()
         
-        # Set up some window properties
-        screen_size = Widgets.QDesktopWidget().screenGeometry()
+        # Set up some basic window properties
+        pg.setConfigOptions(imageAxisOrder="row-major")
+        screen_size = QtWidgets.QDesktopWidget().screenGeometry()
         win_h = int(screen_size.height() * 0.8)
         win_w = int(screen_size.width() * 0.9)
         self.setGeometry(50,50,win_w,win_h)
+        self.setStyleSheet('background:black')
+        self.stylePressed = ("QPushButton {Text-align: left; "
+                             "background-color: rgb(55,55,138); "
+                             "color:white;}")
+        self.styleUnpressed = ("QPushButton {Text-align: left; "
+                               "background-color: rgb(50,50,50); "
+                               "color:white;}")
+        self.styleInactive = ("QPushButton {Text-align: left; "
+                              "background-color: rgb(120,120,120); "
+                              "color:gray;}")
+        self.setWindowTitle('Activity Viewer')
         self.initUI()
         
     def initUI(self):
