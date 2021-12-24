d47c6d6410ea0c6fc52c5afff71a3ebdf565b011
Jake Wright <wrightwj8@gmail.com>
2021-12-24 14:00:50 -0500

    initial commit

---
+++ Viewer/styles.py
@@ -0,0 +1,76 @@
+''' Module containing all the styles for Activity_Viewer
+    GUI widgets'''
+
+def menuStyle():
+    # Style for main menu bar
+    menu_style = ('''QMenuBar {
+                                background-color: #132743;
+                                color:white;}
+                     QMenuBar::item {
+                                background-color: #132743;
+                                color:white;}
+                     QMenuBar::item:selected {
+                                background-color: #02449B;}
+                     QMenu {
+                                background-color: #132743;
+                                color:white;}
+                     QMenu::item {
+                                background-color: #132743;
+                                color:white;}
+                     QMenu::item:selected {
+                                background-color: #02449B;
+                                color:white;}''')
+    return menu_style
+
+def sliderStyle():
+    # Style for slider
+    slider_style = ('''QScrollBar:horizontal {background-color:#131416;
+                                              border: 1px solid #24272D;
+                                              margin:0px 20px 0px 20px}
+                       QScrollBar::handle:horizontal {background:#24272D;
+                                                      color:white}
+                       QScrollBar::add-line:horizontal {background:#24272D;
+                                                        width:15px;
+                                                        subcontrol-position: right;
+                                                        subcontrol-origin: margin}
+                       QScrollBar::sub-line:horizontal {background:#24272D;
+                                                        width:15px;
+                                                        subcontrol-position: left;
+                                                        subcontrol-origin: margin}
+                       QScrollBar:left-arrow:horizontal,QScrollBar:right-arrow:horizontal{
+                                                        width:3px;
+                                                        height:3px;
+                                                        background:white}''')
+    return slider_style
+
+def roiFrameStyle():
+    # Style for roi button frame
+    roi_frame_style = ('''QGroupBox {
+                                    font:bold;
+                                    background:black;
+                                    color:white;
+                                    border:2px solid #132743;
+                                    border-radius: 6px;
+                                    margin-top: 6px}
+                           QGroupBox::title {
+                                    subcontrol-origin:margin;
+                                    subcontrol-position:top;
+                                    padding:0 3px 0 3px}''')
+    return roi_frame_style
+
+def roiBtnStyle():
+    # Style for roi buttons
+    roi_btn_style = ('''QPushButton {
+                                     background:#132743;
+                                     color:white;
+                                     border-radius: 4px;
+                                     text-align:center;
+                                     border-style:outset;
+                                     border-width: 0.5px;
+                                     border-color:#24272D
+                                     }
+                        QPushButton:pressed {
+                                     background:#02449B;
+                                     color:white
+                                     } ''')
+    return roi_btn_style
