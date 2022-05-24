468663d5e0e6b44d25d307af49caaef2df2d68b3
Jake Wright <wrightwj8@gmail.com>
2022-05-24 12:59:57 -0700

    Added getting pixel maps for each roi from avg image

--- Viewer/spine_volume.py
+++ Viewer/spine_volume.py
@@ -3,8 +3,7 @@
 import os
 
 import numpy as np
-
-import images
+from skimage import io as sio
 
 
 def calculate_spine_volume(parent):
@@ -13,6 +12,30 @@ def calculate_spine_volume(parent):
     # Get average image projection to use for volume estimation
     avg_projection = get_total_avg_projection(parent)
 
+    # Get ROI pixels from the avg projection image
+    roi_pixels = {}
+    for key, value in parent.ROIs.items():
+        if key != "Soma":
+            if key == "Background" or key == "Spine":
+                pixels = []
+                for v in value:
+                    p = v.roi.getArrayRegion(
+                        arr=avg_projection, img=parent.current_image, axes=(0, 1)
+                    )
+                    pixels.append(p)
+                roi_pixels[key] = pixels
+            elif key == "Dendrite":
+                dend_pixels = []
+                for v in value:
+                    poly_pixels = []
+                    for poly in v.roi.poly_rois:
+                        p = poly.getArrayRegion(
+                            arr=avg_projection, img=parent.current_image, axes=(0, 1)
+                        )
+                        poly_pixels.append(p)
+                    dend_pixels.append(poly_pixels)
+                roi_pixels[key] = dend_pixels
+
 
 def get_total_avg_projection(parent):
     """Helper function to get the average projection across all tif images"""
@@ -20,4 +43,16 @@ def get_total_avg_projection(parent):
     image_files = [
         img for img in os.listdir(parent.image_directory) if img.endswith(".tif")
     ]
+    frame_tracker = 0
+    summed_files = []
+    for image in image_files:
+        image = sio.imread(
+            os.path.join(parent.image_directory, image), plugin="tifffile"
+        )
+        summed_image = np.sum(image, axis=0)
+        frame_tracker = frame_tracker + np.shape(image)[0]
+        summed_files.append(summed_image)
 
+    average_projection = np.sum(summed_files, axis=0) / frame_tracker
+
+    return average_projection
