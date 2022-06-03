2473b56010a83cb5a2f0a1ebebbec3943009bd44
Jake Wright <wrightwj8@gmail.com>
2022-06-03 14:02:34 -0700

    updated version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="Activity_Viewer",
-    version="0.0.1",
+    version="0.0.2",
     description="GUI to extract fluorescence for nerual activity videos",
     long_description=readme(),
     long_description_content_type="text/markdown",
