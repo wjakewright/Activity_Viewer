df381701fd1a4d293b0cbd552e1d4eb7766d83eb
Jake Wright <wrightwj8@gmail.com>
2022-06-30 13:30:37 -0700

    fixed error when only one spine listed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="Activity_Viewer",
-    version="0.0.33",
+    version="0.0.34",
     description="GUI to extract fluorescence for nerual activity videos",
     long_description=readme(),
     long_description_content_type="text/markdown",
