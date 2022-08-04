7365c3ebecab1ffa8475c27fd193929fe00625b5
Jake Wright <wrightwj8@gmail.com>
2022-08-04 16:01:18 -0700

    Made getting mouse name more generic

--- Activity_Viewer/ROIs.py
+++ Activity_Viewer/ROIs.py
@@ -466,7 +466,7 @@ def save_ROIs(parent):
     save_dialog.setAcceptMode(QFileDialog.AcceptSave)
     save_dialog.setDirectory(r"C:\Users\Jake\Desktop\Analyzed_data\individual")
     year = time.ctime(os.path.getctime(parent.filename))[-2:]
-    mouse = re.search("JW[0-9]{3}", parent.filename).group()
+    mouse = re.search("[A-Z]{2}[0-9]{3}", parent.filename).group()
     date = year + re.search("[0-9]{4}", parent.filename).group()
     sname = f"{mouse}_{date}_imaging_data"
     save_dialog.selectFile(sname)
