cc90334e1e8115f5d5f2e23672a5193f573bf3db
Jake Wright <wrightwj8@gmail.com>
2022-08-05 10:36:43 -0700

    Made getting filename more generic

--- Activity_Viewer/output.py
+++ Activity_Viewer/output.py
@@ -69,7 +69,7 @@ def output_data(parent):
     save_dialog.setAcceptMode(QFileDialog.AcceptSave)
     save_dialog.setDirectory(r"C:\Users\Jake\Desktop\Analyzed_data\individual")
     year = time.ctime(os.path.getctime(parent.filename))[-2:]
-    mouse = re.search("JW[0-9]{3}", filename).group()
+    mouse = re.search("[A-Z]{2}[0-9]{3}", filename).group()
     date = year + re.search("[0-9]{4}", filename).group()
     sname = f"{mouse}_{date}_imaging_data"
     save_dialog.selectFile(sname)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="Activity_Viewer",
-    version="0.0.52",
+    version="0.0.53",
     description="GUI to extract fluorescence for nerual activity videos",
     long_description=readme(),
     long_description_content_type="text/markdown",
